9c818e64c0f308e39aa83519ceb70a25a654981e
Simon Andres <simonandres@MacBook-Air-de-Simon.local>
2026-05-13 11:05:49 -0500

    fix duplicate cors

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-const cors = require('cors');
-
-app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
